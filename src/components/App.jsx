e2b61fd46d3d8ee5ef1b9b4cac87ed8154f8ed88
Devendra Singh <devsinghindra@gmail.com>
2020-04-25 14:45:23 +0530

    added cards and its styles and displayed in app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,25 @@ import React from "react";
 import Cards from "./Cards/Cards";
 import Chart from "./Chart/Chart";
 import CountryPicker from "./CountryPicker/CountryPicker";
-import Styles from "./App.module.css";
+import styles from "./App.module.css";
+import { fetchData } from "../api";
 
 class App extends React.Component {
+
+    state = {
+        data: {}
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+        console.log(fetchedData);
+        this.setState({ data: fetchedData });
+    }
     render() {
+        const { data } = this.state;
         return (
-            <div className={Styles.container}>
-                <h1>App</h1>
-                <Cards />
+            <div className={styles.container}>
+                <Cards data={data} />
                 <CountryPicker />
                 <Chart />
             </div>
